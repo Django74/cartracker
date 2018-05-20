9da529edd19f96d95154ab3cf084a243f82f5312
Vincent Truong <vtruong74@outlook.com>
2018-05-20 17:48:39 -0600

    Add car controller file

---
+++ controllers/cars.js
@@ -0,0 +1,72 @@
+let CarsService = require('../services/cars');
+
+class CarsController {
+    constructor(router) {
+        this.router = router;
+        this.registerRoutes();
+    }
+
+    registerRoutes() {
+        this.router.get('/', this.getCars.bind(this));
+        this.router.get('/:id', this.getSingleCar.bind(this));
+        this.router.post('/', this.postCars.bind(this));
+        this.router.put('/id', this.putCars.bind(this));
+        this.router.delete('/id', this.deleteCars.bind(this));
+    }
+
+    getCars(req, res) {
+        let cars = CarsService.getCars();
+        res.send(cars);
+    }
+
+    getSingleCar(req, res) {
+        let id = req.params.id;
+        let car = CarsService.getSingleCar();
+
+        if (!car) {
+            res.sendStatus(404);
+        } else {
+            res.send(car);
+        }
+    }
+
+    postCars(req, res) {
+        let carInfo = req.body;
+
+        if (CarsService.addCar(carInfo)) {
+            res.setHeader('Location', '/cars/' + carInfo.id);
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(500);
+        }
+    };
+
+    putCars(req, res) {
+        let id = req.params.id;
+        let existingCar = CarsService.getSingleCar(id);
+
+        if (!existingCar) {
+            let carInfo = req.body;
+            carInfo.id = id;
+            if (CarsService.addCar(carInfo)) {
+                res.setHeader('Location', '/cars/' + id);
+                res.sendStatus(201);
+            } else {
+                res.sendStatus(500);
+            }
+        } else {
+            if (CarsService.updateCar(id, req.body)) {
+                res.sendStatus(204);
+            } else {
+                res.sendStatus(404);
+            }
+        }
+    }
+
+    deleteCars(req, res) {
+        let cars = CarsService.deleteCars();
+        res.send(cars);
+    }
+}
+
+module.exports = CarsController;
