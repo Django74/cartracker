39cea55579c0ceb7a5289c8381623d05b11c6d8a
Vincent Truong <vtruong74@outlook.com>
2018-05-21 00:43:23 -0600

    Add in the reamining REST api calls

--- controllers/cars.js
+++ controllers/cars.js
@@ -10,64 +10,72 @@ class CarsController {
         this.router.get('/', this.getCars.bind(this));
         this.router.get('/:id', this.getSingleCar.bind(this));
         this.router.post('/', this.postCars.bind(this));
-        this.router.put('/id', this.putCars.bind(this));
-        this.router.delete('/id', this.deleteCars.bind(this));
+        this.router.put('/:id', this.putCars.bind(this));
+        this.router.delete('/:id', this.deleteCars.bind(this));
     }
 
     getCars(req, res) {
-        CarsService.getCars((result) => {
-            res.json(result);
+        CarsService.getCars((cars) => {
+            res.json(cars);
         });
     }
 
     getSingleCar(req, res) {
         let id = req.params.id;
-        let car = CarsService.getSingleCar(id);
-
-        if (!car) {
-            res.sendStatus(404);
-        } else {
-            res.send(car);
-        }
+        CarsService.getSingleCar(id, (car) => {
+            if (!car) {
+                res.sendStatus(404);
+            } else {
+                res.json(car);
+            }
+        });
     }
 
     postCars(req, res) {
         let carInfo = req.body;
 
-        if (CarsService.addCar(carInfo)) {
-            res.setHeader('Location', '/cars/' + carInfo.id);
-            res.sendStatus(200);
-        } else {
-            res.sendStatus(500);
-        }
+        CarsService.addCar(carInfo, (result) => {
+            if (result) {
+                res.setHeader('Location', '/cars/' + carInfo.id);
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(500);
+            }
+        });
     };
 
     putCars(req, res) {
         let id = req.params.id;
-        let existingCar = CarsService.getSingleCar(id);
+        CarsService.getSingleCar(id, (existingCar) => {
+            if (!existingCar) {
+                let carInfo = req.body;
+                carInfo.id = id;
+                CarsService.addCar(carInfo, (result) => {
+                    if (result) {
+                        res.setHeader('Location', '/cars/' + carInfo.id);
+                        res.sendStatus(201);
+                    } else {
+                        res.sendStatus(500);
+                    }
+                });
+            } else {
+                CarsService.updateCar(id, req.body, (result) => {
+                  if (result) {
+                      res.sendStatus(204);
 
-        if (!existingCar) {
-            let carInfo = req.body;
-            carInfo.id = id;
-            if (CarsService.addCar(carInfo)) {
-                res.setHeader('Location', '/cars/' + id);
-                res.sendStatus(201);
-            } else {
-                res.sendStatus(500);
+                  } else {
+                      res.sendStatus(404);
+                  }
+                });
             }
-        } else {
-            if (CarsService.updateCar(id, req.body)) {
-                res.sendStatus(204);
-            } else {
-                res.sendStatus(404);
-            }
-        }
+        });
     }
 
     deleteCars(req, res) {
         let id = req.params.id;
-        let cars = CarsService.deleteCars(id);
-        res.send(cars);
+        CarsService.deleteCars(id, (result) => {
+            res.send(result);
+        });
     }
 }
 
--- services/cars.js
+++ services/cars.js
@@ -2,7 +2,7 @@ let db = require('../db');
 
 class CarsService {
     getCars(callback) {
-        let sql = "SELECT * from cars";
+        let sql = "SELECT * FROM cars";
         db.query(sql, (err, results) => {
            if (err) {
                throw err;
@@ -12,16 +12,56 @@ class CarsService {
         });
     }
 
-    getSingleCar(id) {
+    getSingleCar(id, callback) {
+        let sql =   "SELECT * FROM cars WHERE id=?";
+        db.query(sql, [id], (err, results) => {
+            if (err) {
+                throw err;
+            } else {
+                callback(results);
+            }
+        });
     }
 
-    addCar(carInfo) {
+    addCar(carInfo, callback) {
+        let sql = `INSERT INTO cars
+                    (make, model, year, type, mileage)
+                    VALUES (?, ?, ?, ?, ?)`;
+        db.query(sql, [carInfo.make, carInfo.model, carInfo.year, carInfo.type, carInfo.mileage], (err, results) => {
+            if (err) {
+                throw err;
+            } else {
+                callback(results);
+            }
+        });
     };
 
-    updateCar(id, carInfo) {
+    updateCar(id, carInfo, callback) {
+        let sql = `UPDATE cars
+                    SET make=?,
+                        model=?,
+                        year=?,
+                        type=?,
+                        mileage=?
+                    WHERE id = ?`;
+        db.query(sql, [carInfo.make, carInfo.model, carInfo.year, carInfo.type, carInfo.mileage, id], (err, results) => {
+            if (err) {
+                throw err;
+            } else {
+                callback(results);
+            }
+        });
     }
 
-    deleteCars(id) {
+    deleteCars(id, callback) {
+        let sql = "DELETE FROM cars WHERE id = ?";
+        db.query(sql, [id], (err, results) => {
+            if (err) {
+                throw err;
+            } else {
+                callback(results);
+            }
+        });
     }
 }
 
