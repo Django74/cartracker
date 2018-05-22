5c85d3835dc7901755435c4733fece3c6bae16d6
Vincent Truong <vtruong74@outlook.com>
2018-05-21 23:42:25 -0600

    Change db name to heroku db

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ let connection = mysql.createPool({
     host: 'us-cdbr-iron-east-04.cleardb.net',
     user: 'b3efec29d8ab53',
     password: '7b3a6087',
-    database: 'curve-dental'
+    database: 'heroku_91a452b3cb219c4'
 });
 module.exports = connection;
