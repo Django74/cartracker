8e494f8460768980dbc969f9be9d4785e1d3d4cc
Vincent Truong <vtruong74@outlook.com>
2018-05-22 03:25:29 -0600

    add support for cors header

--- app.js
+++ app.js
@@ -25,6 +25,23 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// set up cors headers
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
 // set up routes
 let carsRouter = express.Router();
 app.use('/cars', carsRouter);
