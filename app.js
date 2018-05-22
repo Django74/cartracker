c31cd3974517180c1810d6ad59daef9bf187b9b5
Vincent Truong <vtruong74@outlook.com>
2018-05-22 03:27:03 -0600

    change to using cors middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let bodyParser = require('body-parser');
+let cors = require('cors');
 
 let CarsController = require('./controllers/cars');
 let TasksController = require('./controllers/tasks');
@@ -24,23 +25,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// set up cors headers
-let allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.use(allowCrossDomain);
+app.use(cors());
 
 // set up routes
 let carsRouter = express.Router();
