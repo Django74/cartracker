782b761e8013098aab71d1970345e15c1bb1091c
Vincent Truong <vtruong74@outlook.com>
2018-05-21 23:19:03 -0600

    Add .sql file and update db to use heroku

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
 let mysql = require('mysql');
 let connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'b3efec29d8ab53',
+    password: '7b3a6087',
     database: 'curve-dental'
 });
 module.exports = connection;
